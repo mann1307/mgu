bf9ef4d7ecd5eb93567a6bb14ba734d0b1ee2bd9
mann1307 <odnoamanbaev@gmail.com>
2014-10-26 15:38:30 +0600

    Romove roots-sass folder

---
+++ themes/roots-sass/front-page.php
@@ -0,0 +1,221 @@
+<section class=" frontpage-big-image">
+	<div class="container">
+		<div class="row">
+			<!-- WELCOME -->
+			<div class="col-md-6 welcome">
+				<div class="welcome_aim text-center">
+					Наша цель - обеспечить неразрывную <br>
+					связь между поколениями выпускников<br> 
+					и построить надёжную платформу<br> 
+					для крепкой дружбы и<br> 
+					взаимовыгодного развития.
+				</div>
+			</div> <!-- CLOSE WELCOME -->
+			<!-- SIGN UP FORM -->
+			<div class="col-md-5  col-md-offset-1 form-sign-up">
+				<form action="">
+					<div class="form-group">
+						<input type="text" class="form-control" placeholder="Ф.И.О.">
+					</div>
+					<div class="form-group ">
+						<input type="text" class="form-control" placeholder="ФАКУЛЬТЕТ">
+					</div>
+					<div class="form-group ">
+						<input type="text" class="form-control" placeholder="ГОД ВЫПУСКА">
+					</div>
+					<div class="form-group ">
+						<input type="text" class="form-control" placeholder="EMAIL">
+					</div>
+					<div class="form-group  text-center">
+						<button type="submit" class="btn btn-default ">вступить</button>
+					</div>
+				</form>
+			</div> <!-- SIGN UP FORM -->
+
+		</div>
+	</div>
+
+</section>
+<!-- EVENTS -->
+<section class="main-page-events">
+	<header class="text-center main-page-header"><h1>МЕРОПРИЯТИЯ</h1></header>
+	<div class="container">
+		<div class="events-schedule " >
+			<ul class="schedule-header nav nav-tabs  row" role="tablist">
+				<li class="active" ><a href="#preview" class="review-link active" role="tab" data-toggle="tab">АНОНСЫ</a></li>
+				<li><a href="#report" class="events-link" role="tab" data-toggle="tab">ОТЧЕТЫ</a></li>				
+			</ul>
+			<div class="tab-content">
+				<div class="tab-pane fade in active" id="preview"> 
+				<div class="schedule-event row" >
+					
+					<div class="event-date col-md-1">03.10</div>
+					<div class="event-info col-md-10">
+						<h2>Научная конференция</h2>
+						<p>«Российское философское сообщество: история, современное состояние,
+							ерспективы развития»</p>
+						</div>
+						<a href="" class="col-md-1 event-more more"></a>
+					
+				</div>
+				</div>
+				
+				<div class="tab-pane fade" id="report">
+				<div class="list-report row">
+					
+						<div class="event-date col-md-1">03.10</div>
+						<div class="event-info col-md-10">
+							<h2>Отчет о  научной конференции</h2>
+							<p>«Российское философское сообщество: история, современное состояние,
+								ерспективы развития»</p>
+							</div>
+							<a href="" class="col-md-1 event-more more"></a>
+				</div>
+				<div class="list-report  row">
+					
+						<div class="event-date col-md-1">03.10</div>
+						<div class="event-info col-md-10">
+							<h2>Отчет о  научной конференции</h2>
+							<p>«Российское философское сообщество: история, современное состояние,
+								ерспективы развития»</p>
+							</div>
+							<a href="" class="col-md-1 event-more more"></a>
+
+					
+
+				</div>
+				</div>
+			</div>
+
+			</div>
+		</div>
+	</section>
+
+	<section class="main-page-info">
+		<header class="text-center main-page-header"><h1>НОВОСТИ</h1></header>
+		<div class="container">
+			<div class="row">
+
+				<div class="main-page-news-list col-md-8">
+
+
+					<div class="main-page-news row">
+						<div class="news-date">
+							9 августа, 2014
+						</div>
+
+
+						<div class="row">
+							<div class="news-thumbnail col-md-5">
+								<img src="<?php theImgUrl('news1.jpg');?>" alt="">
+							</div>
+
+							<div class="news-text  col-md-7">
+								<h2>Республиканский конгресс политологов</h2>
+								<p> 
+									Директор Казахстанского филиала МГУ имени М.В. Ломоносова, 
+									профессор Александр Владимирович Сидорович  принял участие
+									в работе Республиканского форума «Казахстанский 
+									путь – Стратегия-2050», организованного Республиканским 
+									общественным объединением «Конгресс политологов 
+									Казахстана» 8 сентября в Назарбаев Университете. А.В.Сидорович
+									выступил с пленарным докладом на тему «Новая взаимосвязь 
+									экономической науки и политологии в стратегии развития страны».
+								</p>
+							</div>
+
+
+
+						</div>
+						
+					</div>
+
+
+					<div class="main-page-news row">
+						<div class="news-date">
+							9 августа, 2014
+						</div>
+
+
+						<div class="row">
+							<div class="news-thumbnail col-md-5">
+								<img src="<?php theImgUrl('news1.jpg');?>" alt="">
+							</div>
+
+							<div class="news-text  col-md-7">
+								<h2>Республиканский конгресс политологов</h2>
+								<p> 
+									Директор Казахстанского филиала МГУ имени М.В. Ломоносова, 
+									профессор Александр Владимирович Сидорович  принял участие
+									в работе Республиканского форума «Казахстанский 
+									путь – Стратегия-2050», организованного Республиканским 
+									общественным объединением «Конгресс политологов 
+									Казахстана» 8 сентября в Назарбаев Университете. А.В.Сидорович
+									выступил с пленарным докладом на тему «Новая взаимосвязь 
+									экономической науки и политологии в стратегии развития страны».
+								</p>
+							</div>
+
+
+
+						</div>
+						
+					</div>
+					
+
+
+
+				<div class="row">
+					<div class="all-news text-center"><a data-hover="все события"
+					 href="">ВСЕ СОБЫТИЯ</a></div>
+				</div>
+				</div>
+
+
+				<div class="main-page-initiators col-md-3 col-md-offset-1 text-center">
+
+					<h1>Инициаторы проекта</h1>
+
+					<div class="initiator ">
+						<div class="initiator-thubnail "><img src="<?php theImgUrl('initiator.png');?>" alt=""></div>
+						<div class="initiator-text ">
+							<h2>Евгений Кравченко</h2>
+							<p>Комерческий директор <br>
+								ТОО “IntelResurs” </p>
+							</div>
+						</div>
+					
+					<div class="initiator ">
+						<div class="initiator-thubnail "><img src="<?php theImgUrl('initiator.png');?>" alt=""></div>
+						<div class="initiator-text ">
+							<h2>Евгений Кравченко</h2>
+							<p>Комерческий директор <br>
+								ТОО “IntelResurs” </p>
+							</div>
+					</div>
+
+					<div class="initiator ">
+						<div class="initiator-thubnail "><img src="<?php theImgUrl('initiator.png');?>" alt=""></div>
+						<div class="initiator-text ">
+							<h2>Евгений Кравченко</h2>
+							<p>Комерческий директор <br>
+								ТОО “IntelResurs” </p>
+							</div>
+					</div>
+
+
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<div class="journal text-center">
+			<a href="" class="journal-download ">СКАЧАТЬ ЖУРНАЛ</a>
+		</div>
+
+		<div class="text-center footer">
+			<div class="site">www.msu.ru</div>
+			<div class="univers">КАЗАХСТАНСКИЙ ФИЛИАЛ МГУ им. М.В.ЛОМОНОСОВА</div>
+			<div class="address">адрес: Кажымукана, 11</div>
+			<div class="tel">контакты: +7(7172) 35-34-05, 35-34-81, 35-43-87</div>
+		</div>
